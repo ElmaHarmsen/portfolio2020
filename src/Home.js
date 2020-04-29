c3e14119abfae26ac6074d013af8059e9a962ef0
Elma Harmsen <elmaharmsen01@gmail.com>
2020-04-29 17:57:16 +0200

    Continued working on design:
    Navigation and other sections have box shadow instead of component double border.
    Started with the content of the skills section.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Home.scss';
-import DoubleBorder from './components/DoubleBorder.js';
 import Navigation from './components/Navigation.js';
 import Header from './components/Header.js';
 import About from './components/About.js';
@@ -13,11 +12,9 @@ function Home() {
     <div className="App">
       <Navigation /> {/* Top bar with current page tekst and nav menu. About 200px*/}
       <Header /> {/* First page after animation */}
-      <DoubleBorder /> {/* The DoubleBorder represents the 2 greens lines as a seperator between sections/components. */}
       <About /> {/* About me page with the sliding paragraphs and an image. */} 
       <Skills /> {/* Skills page with 3 items and these are switching content. */}
       <Projects /> {/* Projects page with 5 projects and a changing image, with the stack underneath the project link. */}
-      <DoubleBorder /> {/* The DoubleBorder represents the 2 greens lines as a seperator between sections/components. */}
       <Footer /> {/* Small, like 15% height section with contact info, is a link that links to the contact component. About 150px */}
     </div>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import NextArrow from '../components/NextArrow.js';
+import '../Skills.scss';
 
 function Skills() {
   return (
-    <section>
-      
+    <section className="Skills">
+      <h1>let me tell you what i can do<span>.</span></h1>
+      <div className="the-skills">
+        <h2>HTML</h2>
+        <h2>JavaScript</h2>
+        <h2>Vue</h2>
+      </div>
+      <NextArrow />
     </section>
   );
 }
