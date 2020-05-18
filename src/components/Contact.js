4136524fa085b626f9fffba397fb992e6cd90b4c
Elma Harmsen <elmaharmsen01@gmail.com>
2020-05-18 17:35:30 +0200

    Scaling to lg contact page, after underline in middle:
    Made contact page and footer for lg screen.
    Also changed the phonenumber to resume/cv.
    Changed all but projects after underline to the center instead
    of the left, and let it start from 20%.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,19 +5,21 @@ function Contact(props) {
   return (
     <section className={props.isContactOpen ? "Contact open" : "Contact"}>
       <div className="contact-content">
-        <div className="contact-close" onClick={props.closeContact}>
-          <span></span>
-          <span></span>
+        <div className="contact-header">
+          <div className="contact-close" onClick={props.closeContact}>
+            <span></span>
+            <span></span>
+          </div>
+          <h2>Hey there, please use one of these and let's have a talk!</h2>
         </div>
-        <h2 className="contact-header">Hey there, please use one of these and let's have a talk!</h2>
         <div className="contact-info">
-          <h2><span>(+31)</span> 06 23957767</h2>
-          {/* <span className="line-inbetween"></span> */}
-          <h2>elmaharmsen<span>01</span> @gmail.com</h2>
-          {/* <span className="line-inbetween"></span> */}
+          <a href="https://drive.google.com/file/d/15qDJ20KCGCKTYGOEcFyto52qiqb0NFcB/view?usp=sharing" target="blank">
+            <h3>My Resume/Cv</h3>
+          </a>
+          <h3 className="email-sm">elmaharmsen<span>01</span><br></br>@gmail.com</h3>
+          <h3 className="email-lg">elmaharmsen<span>01</span>@gmail.com</h3>
           <a href="https://www.linkedin.com/in/elma-harmsen-087872181/" target="blank">
-            <h3>Elma Harmsen <br></br>
-            <span>at</span> LinkedIn</h3>
+            <h3>LinkedIn</h3> {/*Elma Harmsen <br></br> <span>at</span>  */}
           </a>
         </div>
       </div>
