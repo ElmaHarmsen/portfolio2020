c8f8d639dde9fc45b40e814e4bd62f3ed0a57974
Elma Harmsen <elmaharmsen01@gmail.com>
2020-05-28 15:59:05 +0200

    Consider the about finished (LG and XL), denim images.

--- src/components/About.js
+++ src/components/About.js
@@ -12,32 +12,25 @@ function About() {
       imageL: "https://elmaharmsen01.netlify.app/images/me-left.jpg"
     },
     {
-      title: "17.08.2000",
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/1200px-Flag_of_the_Netherlands.svg.png",
-      paragraph: "I first set foot on this world in the village of Haren, in summer of 2000. Soon I had great interest in reading, bikes, and the act of building from Lego.",
-      // imageR: require("../assets/2000x1800-About-book-LG.png"),
-      // imageL: "https://elmaharmsen01.netlify.app/images/me-left.jpg"    
-    },
-    {
       title: "01.09.2017",
       img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/1200px-Flag_of_the_Netherlands.svg.png",
       paragraph: "This is when I started the study program Communication & Multimedia Design at the Hanzehogeschool, in Groningen.",
-      // imageR: "https://elmaharmsen01.netlify.app/images/me-right.jpg",
-      // imageL: require("../assets/2000x1800-About-book-LG.png")
+      imageR: "https://elmaharmsen01.netlify.app/images/me-right.jpg",
+      imageL: "https://elmaharmsen01.netlify.app/images/me-left.jpg"
     },
     {
       title: "21.08.2019",
       img: "https://cdn.webshopapp.com/shops/94414/files/52379582/flag-of-denmark.jpg",
       paragraph: "Here, I joined the autumn semester Web Development at the Business Academy Aarhus, in Denmark.",
-      // imageR: require("../assets/2000x1800-About-denmark-LG.png"),
-      // imageL: "https://elmaharmsen01.netlify.app/images/me-left.jpg"  
+      imageR: require("../assets/Aarhus-Harbor.jpg"),
+      imageL:  require("../assets/Windy-Beach.jpg")
     },
     {
       title: "12.02.2020",
       img: "https://cdn.webshopapp.com/shops/94414/files/52379582/flag-of-denmark.jpg",
       paragraph: "So far, I've spent my free time in 2020 wisely by learning Vue & React, along with playing Old School Runescape and the cardgame Exploding Kittens.",
-      // imageR: "https://elmaharmsen01.netlify.app/images/me-right.jpg",
-      // imageL: require("../assets/2000x1800-About-kittens-LG.png")
+      imageR: "https://elmaharmsen01.netlify.app/images/me-right.jpg",
+      imageL: require("../assets/Prague.jpg"),
     }
   ] //The paragraphs with stuff about me.
 
