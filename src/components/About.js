d11d823a96ef864505953af98c854d7e37a1a55e
Elma Harmsen <elmaharmsen01@gmail.com>
2020-05-29 10:55:58 +0200

    Happy about the design from the about me page.

--- src/components/About.js
+++ src/components/About.js
@@ -7,29 +7,27 @@ function About() {
 
   const aboutParagraphs = [
     {
-      title: "Hii! This is me:",
-      imageR: "https://elmaharmsen01.netlify.app/images/me-right.jpg",
-      imageL: "https://elmaharmsen01.netlify.app/images/me-left.jpg"
+      title: "Hii.",
+      paragraph: "I am a young Web Developer. I am on my way to find a company for my internship, planned in the upcoming semester.",
+      imageR: require("../assets/me-right.jpg"),
+      imageL: require("../assets/me-left.jpg")
     },
     {
       title: "01.09.2017",
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/1200px-Flag_of_the_Netherlands.svg.png",
       paragraph: "This is when I started the study program Communication & Multimedia Design at the Hanzehogeschool, in Groningen.",
-      imageR: "https://elmaharmsen01.netlify.app/images/me-right.jpg",
-      imageL: "https://elmaharmsen01.netlify.app/images/me-left.jpg"
+      imageR: require("../assets/me-right.jpg"),
+      imageL: require("../assets/me-left.jpg")
     },
     {
       title: "21.08.2019",
-      img: "https://cdn.webshopapp.com/shops/94414/files/52379582/flag-of-denmark.jpg",
       paragraph: "Here, I joined the autumn semester Web Development at the Business Academy Aarhus, in Denmark.",
       imageR: require("../assets/Aarhus-Harbor.jpg"),
       imageL:  require("../assets/Windy-Beach.jpg")
     },
     {
       title: "12.02.2020",
-      img: "https://cdn.webshopapp.com/shops/94414/files/52379582/flag-of-denmark.jpg",
-      paragraph: "So far, I've spent my free time in 2020 wisely by learning Vue & React, along with playing Old School Runescape and the cardgame Exploding Kittens.",
-      imageR: "https://elmaharmsen01.netlify.app/images/me-right.jpg",
+      paragraph: "So far, I've spent my free time in 2020 by learning Vue & React, along with playing Old School Runescape, and visiting Prague.",
+      imageR: require("../assets/me-right.jpg"),
       imageL: require("../assets/Prague.jpg"),
     }
   ] //The paragraphs with stuff about me.
@@ -38,28 +36,11 @@ function About() {
     <div className="slide">
       <img className="lg-visible lg-figure-L" src={item.imageL} alt=""/>
       <div className="paragraph-text">
-        <img className="tiny-paragraph-image" src={item.img}/>
         <h2>{item.title}</h2>
         <p>{item.paragraph}</p>
       </div>
       <img className="lg-visible lg-figure-R" src={item.imageR} alt=""/>
     </div>
-    // <div className="slide" key={item.title}>
-    //   <div className="the-images">
-    //     <img className="lg-visible"src={item.imageLG}/>
-    //   </div>
-    //   <div>
-    //     <img className="tiny-paragraph-image" src={item.img}/>
-    //     <h2>{item.title}</h2>
-    //     <p>{item.paragraph}</p>
-    //   </div>
-    //   <div className="the-images">
-    //     <img className="lg-visible"src={item.image}/>
-    //   </div>
-    //   {/* <img className="about-me-image" src={item.image}/>
-    //   <img className="about-me-image md-visible"src={item.imageLG}/>
-    //   <img className="about-me-image lg-visible"src={item.imageLG}/> */}
-    // </div>
   )
 
   useEffect(() => {
@@ -84,9 +65,9 @@ function About() {
       <div className="the-about">
         <div className="the-images img-R">
           <img className="sm-figure sm-visible" src={require("../assets/2000x1800-About-SM.png")}/>
-          <img className="md-visible" src="https://elmaharmsen01.netlify.app/images/me-left.jpg" alt=""/>
+          <img className="md-visible" src={require("../assets/me-left.jpg")} alt=""/>
           <img className="md-visible" src={require("../assets/2000x1800-About-MD.png")}/>
-          <img src="https://elmaharmsen01.netlify.app/images/me-right.jpg" alt=""/>
+          <img src={require("../assets/me-right.jpg")} alt=""/>
         </div>
         <div className="the-paragraphs">
           <div className="paragraphs-slides">
@@ -94,9 +75,6 @@ function About() {
           </div>
           <img className="arrow" onClick={nextSlide}  src={require("../assets/arrow-right.svg")}/>
         </div>
-        {/* <div className="the-images">
-          <img src="https://elmaharmsen01.netlify.app/images/me-left.jpg" alt=""/>
-        </div> */}
       </div>
     </section>
   );
