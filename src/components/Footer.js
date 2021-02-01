546b4b354a9670cecf134b4b49cd3329beebb04a
ElmaHarmsen <elmaharmsen01@gmail.com>
2021-02-01 11:32:32 +0100

    clip path footer sm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,29 +8,24 @@ function Footer() {
   return (
     <section>
       <div className="Footer">
-        <h1 className="contact-title">Explore new Worlds</h1>
-        <h2>
-          ...is the best course to find new opportunities! <br></br>
-          Feel like reaching out? Please use one of these.
-        </h2>
-        <div className={cc(["contact-links", currentColumn && "highlight--" + currentColumn ])}>
-          <a onMouseEnter={() => setColumn(1)} onMouseLeave={() => setColumn(null)} href="https://drive.google.com/file/d/12nMt10qPcNYyFJ9FUwosLf1GufMhQjxD/view?usp=sharing" target="blank"><img src={require("../assets/cv.svg")} /></a>
-          <a onMouseEnter={() => setColumn(2)} onMouseLeave={() => setColumn(null)} href="mailto:elmaharmsen01@gmail.com"><img src={require("../assets/gmail.svg")} /></a>
-          <a onMouseEnter={() => setColumn(3)} onMouseLeave={() => setColumn(null)} href="https://www.linkedin.com/in/elma-harmsen-087872181/" target="blank"><img src={require("../assets/linkedin.svg")} /></a>
+        <div className="contact-header">
+          <h1 className="contact-title">Explore new Worlds</h1>
+          <h2>
+            ...is the best course to find new opportunities! <br></br>
+            Feel like reaching out? Please use one of these.
+          </h2>
         </div>
-        {/* <div className="contact-info">
-          <a className="footer-link" onMouseEnter={() => setColor("transparent")} href="https://drive.google.com/file/d/12nMt10qPcNYyFJ9FUwosLf1GufMhQjxD/view?usp=sharing" target="blank">
-            <img src={require("../assets/cv.svg")} />
-          </a>
-          <a className="footer-link" onMouseEnter={() => setColor("transparent")} href="mailto:elmaharmsen01@gmail.com">
-            <img src={require("../assets/gmail.svg")} />
-          </a>
-          <a className="footer-link" style={{ backgroundColor: currentColor}} onMouseEnter={() => setColor("transparent")} href="https://www.linkedin.com/in/elma-harmsen-087872181/" target="blank">
-            <img src={require("../assets/linkedin.svg")} />
-          </a>
-        </div> */}
+        <div className="contact-links-wrapper">
+          <div className={cc(["contact-links", currentColumn && "highlight--" + currentColumn ])}>
+            <span className="sm-line-divider"></span>
+            <a onMouseEnter={() => setColumn(1)} onMouseLeave={() => setColumn(null)} href="https://drive.google.com/file/d/12nMt10qPcNYyFJ9FUwosLf1GufMhQjxD/view?usp=sharing" target="blank"><img src={require("../assets/cv.svg")} /></a>
+            <span className="sm-line-divider"></span>
+            <a onMouseEnter={() => setColumn(2)} onMouseLeave={() => setColumn(null)} href="mailto:elmaharmsen01@gmail.com"><img src={require("../assets/gmail.svg")} /></a>
+            <span className="sm-line-divider"></span>
+            <a onMouseEnter={() => setColumn(3)} onMouseLeave={() => setColumn(null)} href="https://www.linkedin.com/in/elma-harmsen-087872181/" target="blank"><img src={require("../assets/linkedin.svg")} /></a>
+          </div>
+        </div>
       </div>
-      {/* <img className="footer-world" src={require('../assets/planet.svg')}/> */}
     </section>
   );
 }
